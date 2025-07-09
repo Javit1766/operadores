8d6eb33e9c7b01f9aa7c6fdfa22e6d3d320300ab
Javier <djtam@scl.edu.gt>
2025-07-08 19:16:03 -0600

    todo terminado

---
+++ operadoresDeComparacion/operadoresDeComparacion.js
@@ -0,0 +1,34 @@
+console.log(" [Operadores de Consola] ");
+console.log("-------------------------");
+
+let num1 = 5;
+let num2 = 10;
+
+// Operadores de comparacion
+let estado1 = num1 == num2;
+console.log("Estado 1:", estado1);
+
+let estado2 = num1 <= num2;
+console.log("Estado 2:", estado2);
+
+let estado3 = num1 >= num2;
+console.log("Estado 3:", estado3);
+
+let estado4 = num1 < num2;
+console.log("Estado 4:", estado4);
+
+let estado5 = num1 > num2;
+console.log("Estado 5:", estado5);
+
+let estado6 = num1 != num2;
+console.log("Estado 6:", estado6);
+
+// Operadores logicos 
+let estado7 = num1 != num2;
+console.log("Estado 7:", estado7);
+
+let estado8 = num1 && num2;
+console.log("Estado 8:", estado8);
+
+let estado9 = num1 || num2;
+console.log("Estado 9:", estado9);
---
+++ operadoresMatematicos/main.js
@@ -0,0 +1,17 @@
+let fecha = 2025;
+let edad = 18;
+
+//Fecha de nacimiento
+
+let fechaNacimiento = fecha - edad;
+
+console.log("resultado: ", fechaNacimiento);
+
+// 
+
+let num1 = 10;
+let num2 = 26;
+
+console.log("suma", num1+num2);
+console.log("multiplicacion", num1*num2);
+console.log("division", num1/num2);
